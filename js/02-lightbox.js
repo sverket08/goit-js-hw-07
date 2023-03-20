f3ed00ddb57228ce615a742bff379bf807f20069
Lana Oleksiienko <114947428+sverket08@users.noreply.github.com>
2023-03-20 18:13:17 +0200

    last-comit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,9 +5,10 @@ const galleryEl = document.querySelector('.gallery');
 const addGalleryItems = galleryItems.map(({ preview, original, description }) => `<a class="gallery__item gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`).join("");
 galleryEl.insertAdjacentHTML('beforeend', addGalleryItems);
 
-let gallery = new SimpleLightbox('.gallery__item', {
-    captions: true,
-    captionDelay: 250,
-    captionsData: 'alt',
-    captionPosition: 'bottom',
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
 });
+
